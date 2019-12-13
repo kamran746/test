b71c1e680e7ac2c61096e18a7dd4204fe55c4296
Chamanara <mostafa.chamanara@partner.bmw.de>
2019-12-13 10:35:31 +0100

    add file

--- t.cpp
+++ t.cpp
@@ -59,29 +59,6 @@ string remove_body(const char* src)
     
 }
 
-
-void test1()
-{
-    const std::string s = "Quick brown fox.";
- 
-    std::regex words_regex("[^\\s]+");
-    
-	auto words_begin =         std::sregex_iterator(s.begin(), s.end(), words_regex);
-    
-	auto words_end = std::sregex_iterator();
- 
-    std::cout << "Found " 
-              << std::distance(words_begin, words_end) 
-              << " words:\n";
- 
-    for (std::sregex_iterator i = words_begin; i != words_end; ++i) 
-	{
-        std::smatch match = *i;                                                 
-        std::string match_str = match.str(); 
-        std::cout << match_str << '\n';
-    }  	
-}
-
 #include <fstream>
 string readfile(const string& path)
 {
